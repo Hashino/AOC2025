ca8493ec47816f42767f37896ed2eb17ff263d64
Hashino <iam.ravara@gmail.com>
2025-12-02 19:24:05 -0300

    day one done!

--- 1/src/main.zig
+++ 1/src/main.zig
@@ -1,5 +1,43 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, cruel world.\n", .{});
+const direction = enum { left, right };
+
+var dial: i32 = 50;
+
+var password: i32 = 0;
+
+fn rotate(d: direction, a: i32) void {
+    const amount = @rem(a, 100);
+
+    switch (d) {
+        .left => dial -= amount,
+        .right => dial += amount,
+    }
+
+    if (dial < 0) {
+        dial = 100 + dial;
+    } else if (dial > 99) {
+        dial = dial - 100;
+    }
+
+    if (dial == 0) {
+        password += 1;
+    }
+}
+
+pub fn main() !void {
+    const file = std.fs.cwd().openFile("input", .{}) catch unreachable;
+    defer file.close();
+
+    var buffer: [256]u8 = undefined;
+    var reader = file.reader(&buffer);
+
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        const dir = if (line[0] == 'L') direction.left else direction.right;
+        const amount = std.fmt.parseInt(i32, line[1..], 10) catch unreachable;
+
+        rotate(dir, amount);
+    }
+
+    std.debug.print("{}\n", .{password});
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Advent of Code 2025
 
 [AOC2025](https://adventofcode.com/2025)
+
 language: zig
